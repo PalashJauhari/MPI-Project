42431fc79a9563af64118c475b862d7c8c356750
PalashJauhari <43773070+PalashJauhari@users.noreply.github.com>
2023-01-02 00:21:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from dash import Input, Output, dcc, html
 from masterlayout import masterLayout
 
 app = Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.FONT_AWESOME], use_pages=True)
+server = app.server
 app.layout = masterLayout()
 
 if __name__ == '__main__':
-	app.run_server(port=8050)
+	app.run_server(debug=False)
